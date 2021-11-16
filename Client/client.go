157b55907ecddf7be0f1827dc4791cb21f9b32c8
xZenshin <askoularsen@gmail.com>
2021-11-16 11:13:51 +0100

    client

--- Client/client.go
+++ Client/client.go
@@ -1,5 +1,6 @@
-package Client
+package main
 
+//go mod init is called Client
 import (
 	"bufio"
 	"fmt"
@@ -15,11 +16,12 @@ var (
 )
 
 func main() {
+	/*
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Println("Enter port number (You can only choose between 5000, 5001 and 5002): ")
 	inputPort, _ := reader.ReadString('\n')
 
-	file, err := os.Open("ports.txt")
+	file, err := os.Open("../ports.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -28,22 +30,32 @@ func main() {
 	scanner := bufio.NewScanner(file)
 
 	for scanner.Scan() {
+		line 
 		split := strings.Split(scanner.Text(), " ")
+		fmt.Println(split)
 		if split[0] == inputPort {
 			id, err := strconv.Atoi(split[2])
 			if err != nil {
+				fmt.Println("Big error", err)
 			}
 			node = n.Node{
 				Port:         split[0],
 				NextNodePort: split[1],
 				ID:           id,
 			}
+			fmt.Printf("Starting node with port: %s\tNext nodeport: %s\tID: %d\n", node.Port, node.NextNodePort, node.ID)
 			break
 		}
 	}
+	*/
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter port number (You can only choose between 5000, 5001 and 5002): ")
+	inputPort, _ := reader.ReadString('\n')
+
+	node.Port = inputPort
+	node.NextNodePort = 
 
 	if node.ID != 0 {
-
 		go n.ListenForMessages(node)
 		go node.TryToAccessCriticalSection()
 
